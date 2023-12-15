7dd3f56944714af04b3c4c33c1cfa71ecb6e5943
sidney-pauly <sidney@friedrich-pauly.de>
2023-12-15 05:54:20 +0000

    Added debug log msg

--- app/controller/auth_controller.py
+++ app/controller/auth_controller.py
@@ -71,6 +71,8 @@ async def authorization_code_flow(request: Request) -> TokenPair:
 
     data = str(await request.body(), encoding='utf-8')
 
+    print(f'Using token: {data}')
+
     token = await get_code(data)
 
     if token is None:
