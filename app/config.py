9fc4fb1aaa1e0b14252ad5e23f5e3f29cb805d35
sidney-pauly <sidney@friedrich-pauly.de>
2025-07-02 04:20:31 +0100

    Added mqtt endpoint as config

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     connection_string: str = 'mongodb://localhost:27017'
+    mqtt_endpoint: str = 'localhost'
     auth_private_key_path: str = 'private.pem'
     auth_public_key_path: str = 'public.pem'
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', env_prefix='rss_server_')
